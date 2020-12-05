257114d0f74582060e772eb7118cd8c132cd40da
ddraco-d <ddraco@oa-e1.msk.21-school.ru>
2020-12-05 22:28:59 +0300

    parsing args in progress...

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ddraco <ddraco@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 16:45:02 by ddraco            #+#    #+#             */
-/*   Updated: 2020/12/05 19:35:59 by ddraco           ###   ########.fr       */
+/*   Updated: 2020/12/05 21:32:11 by ddraco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,14 @@ typedef struct      s_data
 char        **parser(char *line, t_data vars);
 int			get_next_line(int fd, char **line);
 char        **semicolon(char *line);
-char        **ft_big_strdup(char **arr, size_t size, int flag);
+char        **ft_realloc_2arr(char **arr, size_t size, int flag);
 int		    get_amount_line(char **lines);
 void	    *ft_realloc(void *ptr, int size, int newsize);
 char        **add_elem_in_arrayStr(char **src, char *elem);
 char        **delete_elem_in_arrayStr(char **src, char *elem);
 char        *find_elem_in_arrayStr(char **src, char *elem, int boolFindPart);
 char        *get_value_from_var(char **envp, char *name_var);
+char        **ft_strdup_2arr(char **arr);
 /* 
 * Commands
 */
--- parser/parser.c
+++ parser/parser.c
@@ -6,16 +6,25 @@
 /*   By: ddraco <ddraco@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 16:48:08 by ddraco            #+#    #+#             */
-/*   Updated: 2020/12/05 19:17:15 by ddraco           ###   ########.fr       */
+/*   Updated: 2020/12/05 22:27:22 by ddraco           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 #include <stdio.h>
 
-void        just_add()
+char        *just_add(char *str, char symb)
 {
-    
+    char    *new_str;
+    int     length;
+
+    length = ft_strlen(str);
+    new_str = ft_realloc(str, length, length + 1);
+    if (length == 0)
+        new_str[0] = symb;    
+    else
+        new_str[length + 1] = symb;
+    return (new_str);
 }
 void        one_comma_worker()
 {
@@ -29,16 +38,17 @@ void        two_comma_worker()
 char        **parser(char *line, t_data vars)
 {
     char    **parsed_by_semicolon;
-    char    **ready_array;
     char    *spec_symbols = "$'\"";
     int     i;
     int     commands_amount;
     int     j;
     int     counter;
+    int     ready_array_size;
+    char    *buffer;
 
     i = 0;
     counter = 0;
-    
+    ready_array_size = 0;
     parsed_by_semicolon = semicolon(line);
     commands_amount = get_amount_line(parsed_by_semicolon);
     // printf("%d", j);
@@ -54,16 +64,20 @@ char        **parser(char *line, t_data vars)
         i = 0;
         while (i < ft_strlen(parsed_by_semicolon[counter]))
         {
+            vars.args = ft_realloc_2arr(vars.args, ready_array_size + 1, 0);
+            while (parsed_by_semicolon[counter][i] != ' ')
+            {
+                if (ft_strchr(spec_symbols, parsed_by_semicolon[counter][i]) == NULL)
+                    just_add(buffer, parsed_by_semicolon[counter][i]);
+                else if (parsed_by_semicolon[counter][i] == '\'')
+                    one_comma_worker();
+                else if (parsed_by_semicolon[counter][i] == '\"')
+                    two_comma_worker();
+                i++;
+            }
             
-            if (ft_strchr(spec_symbols, parsed_by_semicolon[counter][i]) == NULL)
-                just_add();
-            else if (parsed_by_semicolon[counter][i] == '\'')
-                one_comma_worker();
-            else if (parsed_by_semicolon[counter][i] == '\"')
-                two_comma_worker();
-            else if (parsed_by_semicolon[counter][i] == ' ')
-                
-            i++;
+            // else if (parsed_by_semicolon[counter][i] == ' ')
+            ready_array_size++;
         }
         counter++;
     }
